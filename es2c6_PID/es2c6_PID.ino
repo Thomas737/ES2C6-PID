f6362a49606a272770f4be9f585cc697ce3652c2
1001wolves <liam.statters@gmail.com>
2024-11-21 12:31:35 +0000

    2 gauge averaging
    working as of 21/11/2024

--- es2c6_PID/es2c6_PID.ino
+++ es2c6_PID/es2c6_PID.ino
@@ -27,15 +27,17 @@ int pwm_a = 3;  //PWM control for motor (speed, 0-255)
 int dir_a = 2;  //direction control for motor
 
 int strainPin1 = A0;  //pin that measures strain gauge voltage (analogue 0-1023)
-float R1 = 120;       //the resistances are 120 Ohms (confirmed by the technicians)
-float R2 = R1;        //all resistors in the wheatstone bridge are equal
+int strainPin2 = A1;
+float R1 = 120;  //the resistances are 120 Ohms (confirmed by the technicians)
+float R2 = R1;   //all resistors in the wheatstone bridge are equal
 float R3 = R1;
 float Rg_init = R1;  //unstressed resistance of strain gauge (https://www.tml.jp/e/node/155)
 float Vin = 5;       //voltage across the wheatstone bridge supplied by arduino power
 float intS1;         //voltage measured from strain gauge (0-5V)
+float intS2;
 
-float rolling_average;
-float rolling_array[] = { 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 };
+float rolling_array1[] = { 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 };
+float rolling_array2[] = { 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 };
 
 int n_rolling = 0;
 
@@ -44,9 +46,10 @@ void setup() {
   Serial.begin(9600);
 
   pinMode(strainPin1, INPUT);  //taking input for strain gauge
-  pinMode(A2, INPUT);          //potentiometer positional feedback pin :)
-  pinMode(pwm_a, OUTPUT);      //Set control pins to be output for voltage
-  pinMode(dir_a, OUTPUT);      // set control pins to be output for direction
+  pinMode(strainPin2, INPUT);
+  pinMode(A2, INPUT);      //potentiometer positional feedback pin :)
+  pinMode(pwm_a, OUTPUT);  //Set control pins to be output for voltage
+  pinMode(dir_a, OUTPUT);  // set control pins to be output for direction
 
   analogWrite(pwm_a, 0);     //set  motor to not run (0-255)
   digitalWrite(dir_a, LOW);  //set direction to extend (0-1)
@@ -60,19 +63,23 @@ void loop() {
   //
 
   // calibrate strain gauge (find unstressed voltage, and subtract from subsequent measurements)
-  if (analogRead(A2) < 940 & began == 0) {
+  if (analogRead(A2) < 820 & began == 0) {
     analogWrite(pwm_a, 255);
     digitalWrite(dir_a, HIGH);
   } else {
     if (began == 0) {
       analogWrite(pwm_a, 0);
 
-      for (int i = 0; i < 6; i++) {
+      for (int i = 0; i < 4; i++) {
         Serial.println("trying");
         float strainReading1a = analogRead(strainPin1);  //reading in the range 0-1023 (represents 0-5V)
         float strainReading1b = (strainReading1a * 5 / 1023);
         intS1 = strainReading1b;  // unstressed strain reading in voltage units
         Serial.print(intS1);
+        float strainReading2a = analogRead(strainPin2);  //reading in the range 0-1023 (represents 0-5V)
+        float strainReading2b = (strainReading2a * 5 / 1023);
+        intS2 = strainReading2b;  // unstressed strain reading in voltage units
+        Serial.print(intS2);
         delay(500);
       }
       began = 1;
@@ -81,27 +88,37 @@ void loop() {
   if (began == 1) {
     float strainReading1a = analogRead(strainPin1);                //reading in the range 0-1023 (represents 0-5V)
     float strainReading1b = (strainReading1a * 5 / 1023) - intS1;  // strain readings in voltage units
-    int K = 2.1;                                                   //gauge factor (https://www.tml.jp/e/node/155)
-    int rsGain = 495;                                              //gain of the amplifier (AD8426 datasheet p.21; 100Ohm default)
+    float strainReading2a = analogRead(strainPin2);                //reading in the range 0-1023 (represents 0-5V)
+    float strainReading2b = (strainReading2a * 5 / 1023) - intS2;
+    int K = 2.1;       //gauge factor (https://www.tml.jp/e/node/155)
+    int rsGain = 495;  //gain of the amplifier (AD8426 datasheet p.21; 100Ohm default)
 
     float Vout1 = strainReading1b / rsGain;       //calculating the output voltage from the wheatstone bridge 1
     float num1 = (R1 + R2) * Vin * R3;            //numerator of Rg equation
     float denom1 = Vout1 * (R1 + R2) + Vin * R2;  //denominator of Rg equation
     float Rg1 = (num1 / denom1) - R3;             //calculating strain gauge resistance
-    float dR1 = Rg_init - Rg1;          //calculating the change in strain resistance (should probably not be absolute that may break something)
-    rolling_array[n_rolling % 10] = (dR1 / (Rg_init * K));
-    float strain1=0;
+    float dR1 = Rg_init - Rg1;                    //calculating the change in strain resistance (should probably not be absolute that may break something)
+    float Vout2 = strainReading2b / rsGain;       //calculating the output voltage from the wheatstone bridge 1
+    float num2 = (R1 + R2) * Vin * R3;            //numerator of Rg equation
+    float denom2 = Vout2 * (R1 + R2) + Vin * R2;  //denominator of Rg equation
+    float Rg2 = (num2 / denom2) - R3;             //calculating strain gauge resistance
+    float dR2 = Rg_init - Rg2;
+    rolling_array1[n_rolling % 10] = (dR1 / (Rg_init * K));
+    rolling_array2[n_rolling % 10] = (dR2 / (Rg_init * K));
+    float strain1 = 0;
+    float strain2 = 0;
     for (int j = 0; j < 10; j++) {
-        strain1 += rolling_array[j];  // using the strain resistance and dR to calculate the strain (K is gauge factor)
+      strain1 += rolling_array1[j];  // using the strain resistance and dR to calculate the strain (K is gauge factor)
+      strain2 += rolling_array2[j];  // using the strain resistance and dR to calculate the strain (K is gauge factor)
     }
-    strain1 = strain1 / 10;
-
+    strain1 /= 10;
+    strain2 /= 10;
 
     //
     //PID controller
     //
 
-    target = 800 + constrain(strain1 * 1600000, -700, 150);  //sets a target position dependent on strain, within safe bounds for actuator
+    target = 800 + constrain((strain1 - strain2) / 2 * 1800000, -700, 150);  //sets a target position dependent on strain, within safe bounds for actuator
 
     // if (millis()%12000<6000)
     // {target=100;}
@@ -135,14 +152,16 @@ void loop() {
 
     // //debug outputs
     Serial.print(0);  // To freeze the lower limit
-    Serial.print(" ");
-    Serial.print(1000);  // To freeze the upper limit
-    Serial.print(" ");
-    Serial.print(String(strain1, 6));
     Serial.print(" , ");
-    Serial.print(target);
+    Serial.print(1000);  // To freeze the upper limit
+    Serial.print(" , ");
+    Serial.print(800 + constrain(strain1 * 1600000, -700, 150));
     Serial.print(" , ");
     Serial.print(analogRead(A2));
+    Serial.print(" , ");
+    Serial.print(800 - constrain(strain2 * 1600000, -700, 600));
+    Serial.print(" , ");
+    Serial.print(target);
     Serial.print(" , ");
     // Serial.print(millis());
     // Serial.print(" , ");
